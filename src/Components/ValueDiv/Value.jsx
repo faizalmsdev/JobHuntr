e2e58672084461a7f7901129675576d6cc91849b
Dextrodark <faizalmohamed5302002@gmail.com>
2023-08-29 17:40:57 +0530

    Value added

--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -2,8 +2,61 @@
 
     const Value = () => {
     return (
-        <div>
-        VAlue
+        <div className="mb-[4rem] ml-10 mr-10 mt-[6rem]">
+            <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px block">The value that holds us true and to account</h1>
+
+            <div className="grid gap-[10rem] grid-cols-3 items-center">
+                
+            <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+                    <div className="flex items-center gap-3">
+                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+                        <img src="https://img.freepik.com/premium-vector/abstract-modern-colorful-loop-rings-logo-design-template-universal-gradient-color-vector-logotype_708255-356.jpg" alt="images" className="w-[70%]" />
+                        </div>
+
+                        <span className="font-semibold text-textColor text-[18px]">
+                            Simplicity
+                        </span>
+                    </div>
+
+                    <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold">
+                        Things made beautiful simple are at the heart of everything we do.
+                    </p>
+                </div>
+
+
+                <div className="singleGrid rounded-[10px] hover:bg-[#0cd595] p-[1.5rem]">
+                    <div className="flex items-center gap-3">
+                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+                        <img src="https://img.freepik.com/premium-vector/abstract-modern-colorful-loop-rings-logo-design-template-universal-gradient-color-vector-logotype_708255-356.jpg" alt="images" className="w-[70%]" />
+                        </div>
+
+                        <span className="font-semibold text-textColor text-[18px]">
+                            Simplicity
+                        </span>
+                    </div>
+
+                    <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold">
+                        Things made beautiful simple are at the heart of everything we do.
+                    </p>
+                </div>
+
+                <div className="singleGrid rounded-[10px] hover:bg-[#15bdef] p-[1.5rem]">
+                    <div className="flex items-center gap-3">
+                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+                        <img src="https://img.freepik.com/premium-vector/abstract-modern-colorful-loop-rings-logo-design-template-universal-gradient-color-vector-logotype_708255-356.jpg" alt="images" className="w-[70%]" />
+                        </div>
+
+                        <span className="font-semibold text-textColor text-[18px]">
+                            Simplicity
+                        </span>
+                    </div>
+
+                    <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold">
+                        Things made beautiful simple are at the heart of everything we do.
+                    </p>
+                </div>
+
+            </div>
         </div>
     )
     }
